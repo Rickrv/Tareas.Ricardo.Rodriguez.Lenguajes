a9c70c8725bff64ca096a7eac16e9ad2af4b4738
Ricardo <Ricardo@10.20.2.55>
2014-06-09 19:32:30 -0500

    trabajo de la clase del dia 9 de junio

---
+++ cursos.capitulo2.tipos/src/cursos/capitulo2/tipos/ModificadorStatic.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cursos.capitulo2.tipos;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class ModificadorStatic {
+    
+}
