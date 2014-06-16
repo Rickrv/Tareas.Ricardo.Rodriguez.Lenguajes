9e6697662875235dfece7b8962479b78de645d9d
Ricardo <Ricardo@Ricardo>
2014-06-15 22:06:32 -0500

    trabajo de la clase del dia 12 de junio

---
+++ cursos.capitulo2.tipos/src/cursos/capitulo2/tipos/MiThredsillo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cursos.capitulo2.tipos;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class MiThredsillo implements Runnable {
+    
+    public static void main (String algo[]){
+        
+        Runnable r = new MiThredsillo();
+        Thread t1=new Thread (r);
+        Thread t2=new Thread (r);
+        Thread t3=new Thread (r);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+    public void run(){
+        System.out.println("Soy feliz adentro del run ");
+    }
+    
+}
